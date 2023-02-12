53e6c225cd28f09b5e15d72398163823b6384cf6
naonao-aaa <naonao1814naonao@gmail.com>
2023-02-12 09:00:46 +0900

    User情報を削除するAPI

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 //CRUD
+
 //ユーザー情報の更新
 router.put("/:id", async (req, res) => {
   if(req.body.userId === req.params.id || req.body.isAdmin) {
@@ -17,7 +18,21 @@ router.put("/:id", async (req, res) => {
     return res.status(403).json("あなたは自分のアカウントの時だけ情報を更新できます");
   }
 });
+
 //ユーザー情報の削除
+router.delete("/:id", async (req, res) => {
+  if(req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が削除されました");
+    } catch {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("あなたは自分のアカウントの時だけ情報を削除できます");
+  }
+});
+
 //ユーザー情報の取得
 
 module.exports = router;
