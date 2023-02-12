6cc7c7c3067d785a06081e103bd860e8e89c6f2d
naonao-aaa <naonao1814naonao@gmail.com>
2023-02-12 09:37:31 +0900

    特定のUser情報を取得するAPI

--- routes/users.js
+++ routes/users.js
@@ -34,5 +34,14 @@ router.delete("/:id", async (req, res) => {
 });
 
 //ユーザー情報の取得
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
