8d730b8ee97e2e60394a268d22457442a1f6c7ee
naonao-aaa <naonao1814naonao@gmail.com>
2023-02-13 18:11:28 +0900

    マテリアルUIをインストール

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,3 +1,4 @@
+import { Chat, Notifications, Search } from "@mui/icons-material";
 import React from 'react';
 
 export default function Topbar() {
@@ -8,12 +9,19 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
+          <Search className="searchIcon"/>
           <input type="text" className="searchInput" placeholder="探し物は何ですか？" />
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
+        <div className="topbarIconItem">
+          <Chat />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <Notifications />
+          <span className="topbarIconBadge">2</span>
+        </div>
       </div>
       <img src="/assets/person/1.jpeg" alt="" className="topbarImage" />
     </div>
