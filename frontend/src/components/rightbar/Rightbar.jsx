f31a2c72b52bab57fe1cbabe686c56b1a3bdc5f2
naonao-aaa <naonao1814naonao@gmail.com>
2023-03-12 15:39:06 +0900

    Rightbarコンポーネントのスタイリング

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -9,15 +9,22 @@ export default function Rightbar() {
           <img src="assets/star.png" alt="" className="starImg" />
           <span className="eventText"><b>フォロワー限定</b>イベント開催中！</span>
         </div>
-        <img src="assets/event.jpeg" alt="" />
+        <img src="assets/event.jpeg" alt="" className="eventImg" />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="assets/person/1.jpeg" alt="" />
+              <img src="assets/person/1.jpeg" alt="" className="rightbarProfileImg" />
               <span className="rightbarOnline"></span>
             </div>
             <span className="rightbarUsername">Shin Code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tanaka</span>
           </li>
         </ul>
       </div>
